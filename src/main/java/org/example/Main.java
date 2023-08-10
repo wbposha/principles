b071499cc4eaff4fb7f3a678569f189b6e24f7db
wbposha <135736179+wbposha@users.noreply.github.com>
2023-08-11 03:25:56 +0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,9 +12,9 @@ public class Main {
 
 
         post.birthday= new FormDate();
-        post.birthday.day = "13";
-        post.birthday.month = "6";
-        post.birthday.year = "1999";
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
 
     }
 }
